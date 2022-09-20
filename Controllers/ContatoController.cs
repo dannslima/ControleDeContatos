0e91d851183d94990c05d71471cce7e3e3cba387
Danilo Lima <danilo.lima@csjsistemas.com.br>
2022-09-19 21:24:18 -0300

    first commit!

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Data/BancoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+
+
+
+    }
+}
