a3e665fd954d857de1ee9f84476bfae360269d06
Danilo Lima <danilo.lima@csjsistemas.com.br>
2022-10-07 18:21:44 -0300

    usando dataanotations

--- Models/ContatoModel.cs
+++ Models/ContatoModel.cs
@@ -1,10 +1,17 @@
-﻿namespace ControleDeContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Digite o Nome do Contato")]
         public string Nome { get; set; }
+        [Required(ErrorMessage ="Digite o Email do contato")]
+        [EmailAddress(ErrorMessage ="O email não é válido")]
         public string Email { get; set; }
+        [Required(ErrorMessage ="Digite o Celular do contato")]
+        [Phone(ErrorMessage ="O celular informado não é válido")]
         public string Celular { get; set; }
 
     }
